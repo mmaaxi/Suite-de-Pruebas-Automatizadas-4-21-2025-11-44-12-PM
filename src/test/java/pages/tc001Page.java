aa85d5c873be1e035b429153a1c21de0991bd3e4
Maximiliano Reyes López <113549509+mmaaxi@users.noreply.github.com>
2025-04-21 17:44:28 -0600

    agregando src/test/java/pages/tc001Page.java

---
+++ src/test/java/pages/tc001Page.java
@@ -0,0 +1,38 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class tc001Page {
+    WebDriver driver;
+
+    By usernameField = By.id("username");
+    By passwordField = By.id("password");
+    By loginButton = By.id("loginButton");
+    By homePageElement = By.id("homePage");
+    By authenticatedElement = By.id("authenticatedUser");
+
+    public tc001Page(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public boolean isHomePageVisible() {
+        return driver.findElement(homePageElement).isDisplayed();
+    }
+
+    public void enterUsername(String username) {
+        driver.findElement(usernameField).sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        driver.findElement(passwordField).sendKeys(password);
+    }
+
+    public void clickLoginButton() {
+        driver.findElement(loginButton).click();
+    }
+
+    public boolean isUserAuthenticated() {
+        return driver.findElement(authenticatedElement).isDisplayed();
+    }
+}
